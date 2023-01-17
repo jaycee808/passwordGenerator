2904c318cd22d61487951dedbf35b7ce4e63d6d1
jaycee808 <jamie.clark@live.co.uk>
2023-01-17 22:10:48 +0000

    added function to prompt user for password options

--- script.js
+++ script.js
@@ -89,3 +89,38 @@ var upperCasedCharacters = [
 var passwordLength = "";
 var finalPassword = [];
 var passwordChoices = [];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+    var passwordLength = (prompt("What length of password do you want? The password has to have a minimum of 10 characters and a maximum of 64"));
+
+    while(passwordLength < 10 || passwordLength > 64) {
+        alert("Please choose a number between 10 and 64!");
+        // ask user for password length again
+        getPasswordOptions();
+    }
+
+    // Prompts to choose characters to include in password
+    var confirmSpecialCharacters = confirm("Do you want the password to include special characters?");
+    var confirmNumericCharacters = confirm("Do you want the password to include numbers?");
+    var confirmLowerCasedCharacters = confirm("Do you want the password to include lowercase characters?");
+    var confirmUpperCasedCharacters = confirm("Do you want the password to include uppercase characters?");
+
+    // if statement to include selected characters
+    if (confirmSpecialCharacters) {
+        passwordChoices = passwordChoices.concat(specialCharacters);
+    } if (confirmNumericCharacters) {
+        passwordChoices = passwordChoices.concat(numericCharacters);
+    } if (confirmLowerCasedCharacters) {
+        passwordChoices = passwordChoices.concat(lowerCasedCharacters);
+    } if (confirmUpperCasedCharacters) {
+        passwordChoices = passwordChoices.concat(upperCasedCharacters);
+    }
+    
+    // for loop to select random characters
+    for (var i = 0; i < passwordLength; i++) {
+      var finalPassword = passwordChoices[Math.floor(Math.random() * passwordChoices.length)];
+        console.log(finalPassword)
+      //  return(finalPassword);
+    }
+}
